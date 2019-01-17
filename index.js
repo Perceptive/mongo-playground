f13588154420621433c5ed93cc4332d9fcaa31b3
Micah Henning <micah@perceptive.io>
2019-01-16 18:04:43 -0600

    feature(playground): A simple version 1
    - Added MongoDB driver
    - Programmed execution of mongo queries
    - Styled playground page

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoConnection = require('./utils/mongodb.connection');
 
 // Create app
 const app = express();
@@ -6,5 +8,35 @@ const port = 4000;
 
 // Enable serving static files
 app.use(express.static('public'));
+
+// Use convenient middleware to extract fully-parse JSON body
+app.use(bodyParser.json());
+
+// Process mongo commands
+app.post('/', async (req, res) => {
+  const {
+    url, method, collection, data,
+  } = req.body;
+
+  // Connect to mongo instance
+  const db = new MongoConnection(url);
+
+  try {
+    await db.open();
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send(`Error occurred while connecting to database ${url}`);
+  }
+
+  // Execute request
+  try {
+    const result = await db.collection(collection)[method](data ? JSON.parse(data) : {});
+    db.close();
+    return res.status(200).send(JSON.stringify(result));
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send(`Error occurred while executing method "${method}" on collection "${collection}"`);
+  }
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
---
+++ utils/mongodb.connection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+class MongoConnection {
+  constructor(url) {
+    this.MONGO_URL = url || 'mongodb://localhost:27017/anaheim';
+    this.DB_NAME = this.MONGO_URL.replace(/^.+:\/\/.+\/(\w+)[?]?.*/, '$1');
+  }
+
+  async open() {
+    this.client = await MongoClient.connect(this.MONGO_URL, { useNewUrlParser: true });
+    this.db = this.client.db(this.DB_NAME);
+  }
+
+  close() {
+    this.client.close();
+  }
+
+  collection(name) {
+    return this.db.collection(name);
+  }
+}
+
+module.exports = MongoConnection;
